cfb353c16abe7533a594aeddbb255f5a476630ad
Ayola Mgqolozana <mgqolozanaa@gmail.com>
2023-02-05 18:34:14 -0500

    removed key

--- book/settings.py
+++ book/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7n9*$q8n&*d8t5c+p14)%8@vc)wnm5x@52##h1xyoyy&y#g!5j'
+SECRET_KEY = ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from poll import views
 from search import views
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name ='home'),
     path('search/', views.search_book, name = 'search_book'),
+    path('', include('search.url'))
 ]
